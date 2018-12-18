7b0a3df8453ead4b892b48c0bb9e001069d1b455
Daizy <kxania@gmail.com>
2018-12-19 01:02:19 +0300

    Implement pagination

--- scripts/notification.js
+++ scripts/notification.js
@@ -1,28 +1,27 @@
 "use strict";
 
-function nots(parcel_id){
-    var cancel = document.getElementById("cancelParcel");
-    var dropdown = document.getElementById("not");
+(function nots(parcel_id){
+    let cancel = document.getElementById("cancelParcel");
+    let dropdown = document.getElementById("not");
     cancel.onclick = function(){
-        alert("cancel req sent");
         console.log(parcel_id);
         document.getElementById("cancelModal").style.display = 'none';
         document.getElementById("cancelbtn").disabled = true;
-        var not = document.getElementById("nots");
-        var noT = `<a href="#">Cancel order ${parcel_id}</a>`;
+        let not = document.getElementById("nots");
+        let noT = `<a href="#">Order ${parcel_id} cancelled</a>`;
 
         not.innerHTML += noT;
-        var notss = [];
+        let notss = [];
         notss.forEach(noT =>{
             notss.push(noT);
         })
         console.log(notss.length);
         document.getElementById("number").innerHTML = (notss.length);
         if(noT){
-            document.querySelector("#number").style.color = 'blue';
+            document.querySelector("#number").style.color = 'white';
         }
     }
     dropdown.onclick = function(){
         document.getElementById("number").style.display = 'none';
     }
-}
+})();
